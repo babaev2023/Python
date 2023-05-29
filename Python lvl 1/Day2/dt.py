394650a37b7ba7a30fa5708858261dec5f992c9b
babaev2023 <stepan.babaev@gmail.com>
2023-05-29 10:43:30 +0300

    date and time

--- Python lvl 1/Day2/dt.py
+++ Python lvl 1/Day2/dt.py
@@ -3,10 +3,9 @@
 from datetime import datetime, timedelta
 # timedelta хранит секунды, дни, микросекунды
 
-# С какого до какого года 32-битная Unix-эпоха?
 dt0 = datetime(1970, 1, 1)
 
-seconds_max = 2**35
+seconds_max = 2**31
 
 dtmin = dt0 - timedelta(seconds=seconds_max)
 dtmax = dt0 + timedelta(seconds=seconds_max)
